893e466cf67e4b26d8692469612ea55282223b98
Evgen-Georgiev <evgenigg2000@gmail.com>
2024-01-21 00:37:02 +0200

    Added models and defined relationships.

---
+++ app/Models/Payment.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    /**
+     * Set name of the model in the database table.
+     *
+     * @var string $table
+     */
+    protected $table = 'payment';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'booking_id',
+        'amount',
+        'payment_date',
+        'status',
+    ];
+
+    // ...
+    public function booking(): BelongsTo
+    {
+        return $this->belongsTo(Booking::class, 'booking_id');
+    }
+}
---
+++ app/Models/Room.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Room extends Model
+{
+    use HasFactory;
+
+    /**
+     * Set name of the model in the database table.
+     *
+     * @var string $table
+     */
+    protected $table = 'room';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'number',
+        'type',
+        'price_per_night',
+        'status',
+    ];
+
+    public function booking(): HasMany
+    {
+        return $this->hasMany(Booking::class);
+    }
+}
