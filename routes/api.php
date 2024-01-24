0f098347944f3d1304f4ab278d112972e8024f5c
Evgen-Georgiev <evgenigg2000@gmail.com>
2024-01-24 02:04:03 +0200

    Removed unnecessary api routes from api.php.

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/room', [RoomController::class, 'store'])->name('room.store');
     Route::post('/booking', [BookingController::class, 'store'])->name('booking.store');
+    Route::delete('/booking/{booking}', [BookingController::class, 'destroy'])->name('booking.destroy');
     Route::post('/customer', [CustomerController::class, 'store'])->name('customer.store');
     Route::post('/payment', [PaymentController::class, 'store'])->name('payment.store');
     Route::post('/logout', [AuthController::class, 'logout']);
@@ -37,26 +38,19 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::controller(RoomController::class)->group(function() {
     Route::get('/room', 'index')->name('room.index');
     Route::get('/room/{room}', 'show')->name('room.show');
-    Route::put('/room/{room}', 'update')->name('room.update'); // ...
 });
 
 Route::controller(BookingController::class)->group(function() {
     Route::get('/booking', 'index')->name('booking.index');
     Route::get('/booking/{booking}', 'show')->name('booking.show');
-//    Route::post('/booking', 'store')->name('booking.store');
-    Route::put('/booking/{booking}', 'update')->name('booking.update'); // ...
 });
 
 Route::controller(CustomerController::class)->group(function() {
     Route::get('/customer', 'index')->name('customer.index');
     Route::get('/customer/{customer}', 'show')->name('customer.show');
-//    Route::post('/customer', 'store')->name('customer.store');
-    Route::put('/customer/{customer}', 'update')->name('customer.update'); // ...
 });
 
 Route::controller(PaymentController::class)->group(function() {
     Route::get('/payment', 'index')->name('payment.index');
     Route::get('/payment/{payment}', 'show')->name('payment.show');
-//    Route::post('/payment', 'store')->name('payment.store');
-    Route::put('/payment/{payment}', 'update')->name('payment.update'); // ...
 });
